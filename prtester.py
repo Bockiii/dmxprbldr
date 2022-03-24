ace969e9fd3375c8d49a5347b8a7422bed2e1275
Henning Bocklage <henning@bocklage.com>
2022-03-24 19:28:49 +0100

    s

--- prtester.py
+++ prtester.py
@@ -37,7 +37,7 @@ def testBridges(bridges,status):
             if not errormessages:
                 r = requests.get(URL + bridgestring + random.choice(formstrings))
                 pagetext = r.text
-                pagetext.replace("static/HtmlFormat.css","https://feed.eugenemolotov.ru/static/HtmlFormat.css")
+                pagetext = pagetext.replace("static/HtmlFormat.css","https://feed.eugenemolotov.ru/static/HtmlFormat.css")
                 with open(os.getcwd() + "/results/" + bridgeid + '-' + status + '.html', 'w+') as file:
                     file.write(pagetext)
             else:
